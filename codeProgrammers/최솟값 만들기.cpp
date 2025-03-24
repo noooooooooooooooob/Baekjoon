88ce79d512cdd87033157cf9d1cf37767e38a6fc
noooooooooooooooob <168753086+noooooooooooooooob@users.noreply.github.com>
2025-03-24 23:16:22 +0900

    Create 최솟값 만들기.cpp

---
+++ codeProgrammers/최솟값 만들기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(int a, int b) { 
+  return a > b; 
+}
+
+int solution(vector<int> A, vector<int> B)
+{
+    sort(A.begin(), A.end());
+    sort(B.begin(),B.end(),cmp); // sort 나만의 정렬 이용하기
+    int answer = 0;
+    for(int i=0;i<A.size();i++)
+        answer += A[i] * B[i];
+    return answer;
+}
